6e61df1f4a193ac5c3ff22bdb29b9b808521ea12
Colin Hill <cxmhill@gmail.com>
2021-11-20 14:23:55 -0600

    Initial project creation

---
+++ leads/serializers.py
@@ -0,0 +1,7 @@
+form rest_framework import serializers
+from .models import Lead
+
+class LeadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lead
+        fields = ('id', 'name', 'email', 'message')
